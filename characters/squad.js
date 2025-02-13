6075212b9c24e6e502ef1d50dc346fe94408619a
PINARD0 <tjcolomvaquer@gmail.com>
2025-02-13 19:41:30 +0100

    Better pics

--- characters/squad.js
+++ characters/squad.js
@@ -18,10 +18,10 @@ const squadData = {
       "rank": "Gunnery Sergeant",
       "role": "Squad leader, strategist",
       "status": "Active (Halo 5: Guardians)",
-      "image-banner": null,
-      "image-profile": null,
-      "image-1": null,
-      "image-2": null,
+      "image-banner": "../img/buck/buck-banner.webp",
+      "image-profile": "../img/buck/buck-profile.webp",
+      "image-1": "../img/buck/buck-iddle.webp",
+      "image-2": "../img/buck/buck-iddle-weapon.webp",
       "lore": "Edward Malcolm Buck, better known as Buck, is a Gunnery Sergeant and the leader of Alpha-Nine. He is known for his strategic mind and his ability to lead his squad through the most difficult situations. Buck is currently an active member of the Spartan-IV program."
     },
     {
@@ -30,10 +30,10 @@ const squadData = {
       "rank": "Corporal",
       "role": "Explosives specialist",
       "status": "Retired",
-      "image-banner": null,
-      "image-profile": null,
-      "image-1": null,
-      "image-2": null,
+      "image-banner": "../img/dutch/dutch-banner.webp",
+      "image-profile": "../img/dutch/dutch-profile.webp",
+      "image-1": "../img/dutch/dutch-iddle.webp",
+      "image-2": "../img/dutch/dutch-iddle-weapon.webp",
       "lore": "Taylor Henry Miles, better known as Dutch, was a Corporal and the explosives specialist of Alpha-Nine. After the war, Dutch retired from the UNSC and became a private security contractor."
     },
     {
@@ -42,10 +42,10 @@ const squadData = {
       "rank": "Corporal",
       "role": "Pilot, heavy weapons",
       "status": "Imprisoned (Treason)",
-      "image-banner": null,
-      "image-profile": null,
-      "image-1": null,
-      "image-2": null,
+      "image-banner": "../img/mickey/mickey-banner.webp",
+      "image-profile": "../img/mickey/mickey-profile.webp",
+      "image-1": "../img/mickey/mickey-iddle.webp",
+      "image-2": "../img/mickey/mickey-iddle-weapon.webp",
       "lore": "Michael Crespo, better known as Mickey, was a Corporal and the pilot of Alpha-Nine. He was known for his skills as a heavy weapons specialist. Mickey was imprisoned for treason after the events of Halo 3: ODST."
     },
     {
@@ -54,10 +54,10 @@ const squadData = {
       "rank": "Corporal",
       "role": "Marksman, sniper",
       "status": "Active",
-      "image-banner": null,
-      "image-profile": null,
-      "image-1": null,
-      "image-2": null,
+      "image-banner": "../img/romeo/romeo-banner.webp",
+      "image-profile": "../img/romeo/romeo-profile.webp",
+      "image-1": "../img/romeo/romeo-iddle.webp",
+      "image-2": "../img/romeo/romeo-iddle-weapon.webp",
       "lore": "Kojo Agu, better known as Romeo, is a Corporal and the marksman of Alpha-Nine. He is known for his exceptional skills as a sniper. Romeo is currently an active member of the Spartan-IV program."
     },
     {
@@ -66,10 +66,10 @@ const squadData = {
       "rank": "ONI Agent",
       "role": "ONI intelligence officer, mission commander",
       "status": "Active",
-      "image-banner": null,
-      "image-profile": null,
-      "image-1": null,
-      "image-2": null,
+      "image-banner": "../img/dare/dare-banner.webp",
+      "image-profile": "../img/dare/dare-profile.webp",
+      "image-1": "../img/dare/dare-iddle.webp",
+      "image-2": "../img/dare/dare-iddle-weapon.webp",
       "lore": "Veronica Dare is an ONI intelligence officer and the mission commander of Alpha-Nine. She is known for her strategic mind and her ability to lead her squad through the most difficult situations. Dare is currently an active ONI agent."
     }
   ]
